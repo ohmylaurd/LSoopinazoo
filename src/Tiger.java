538aec2a35a85fd076aa755ed9ff305002d36477
lauren spencer <laurenspencer@students.berkeley.net>
2018-04-18 15:38:47 -0700

    done

---
+++ src/Tiger.java
@@ -0,0 +1,30 @@
+public class Tiger {
+   public String name;
+   public String favoriteFood;
+
+    public Tiger(String name){
+        this.name=name;
+        this.favoriteFood="meat";
+
+    }
+
+        public void sleep() {
+            System.out.println(name + " sleeps for 8 hours a day.");
+
+        }
+
+
+        public void eat(String food) {
+            System.out.println(name + " eats " + food );
+            if(food==favoriteFood){
+                System.out.print("YUM!! " + name +" wants more " + food);
+
+            }
+        }
+
+}
+
+
+
+
+
